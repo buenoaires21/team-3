72418d997ba5945c61c2f89ea1910436533e9816
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-04-30 19:27:01 -0300

    front cambios

--- front/src/Components/Register.jsx
+++ front/src/Components/Register.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 import { postData } from '../states/userReducer'
 import { useInput } from '../utils/useInput'
+import axios from 'axios'
 
 const Register = () => {
     const dispatch = useDispatch();
@@ -27,7 +28,9 @@ const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(
+
+        axios.post("alta.php", { name: name.value })
+       /*  dispatch(
             postData({
                 mail: mail.value,
                 password: password.value,
@@ -47,109 +50,108 @@ const Register = () => {
                 education: education.value,
                 workExperience: workExperience.value
             })
-        );
+        ); */
     }
-
 
     return (
         <div>
 
-            <div>
+            <form onSubmit={handleSubmit} >
                 <h1 className='margin-top-form text-center'>Ingrese sus datos</h1>
                 <hr />
-                <form onSubmit={handleSubmit}>
+                <div  >
                     <h2>Usuario</h2>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Email</label>
-                        <input 
-                        {...mail}
-                        type="text
+                        <input
+                            {...mail}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Email" />
                     </div>
 
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Contraseña</label>
-                        <input 
-                        {...password}
-                        type="text
+                        <input
+                            {...password}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
                     </div>
 
 
-                </form>
-                <form>
+                </div>
+                <div>
                     <h2>Datos personales</h2>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Nombre</label>
-                        <input 
-                        {...name}
-                        type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
+                        <input
+                            {...name}
+                            type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre" />
                     </div>
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Apellido</label>
-                        <input 
-                         {...lastName}
-                        type="text
+                        <input
+                            {...lastName}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Apellido" />
                     </div>
 
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Telefono</label>
-                        <input 
-                        {...phone}
-                        type="text
+                        <input
+                            {...phone}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Telefono" />
                     </div>
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">DNI</label>
-                        <input 
-                        {...dni}
-                        type="text
+                        <input
+                            {...dni}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="DNI" />
                     </div>
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Cuil</label>
-                        <input 
-                        {...cuil}
-                        type="text
+                        <input
+                            {...cuil}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Cuil" />
                     </div>
 
 
 
-                </form>
-                <form>
+                </div>
+                <div>
                     <h2>Datos bancarios</h2>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Nombre del banco</label>
-                        <input 
-                        {...bankName}
-                        type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Banco" />
+                        <input
+                            {...bankName}
+                            type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Banco" />
                     </div>
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Tipo de cuenta</label>
-                        <input 
-                         {...bankClass}
-                        type="text
+                        <input
+                            {...bankClass}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Tipo de cuenta" />
                     </div>
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">CBU</label>
-                        <input 
-                        {...cbu}
-                        type="text
+                        <input
+                            {...cbu}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="CBU" />
                     </div>
 
-                </form>
-                <form>
+                </div>
+                <div>
                     <h2>Información Regional</h2>
                     {/* select */}
                     <select {...selectRegion} className="form-select select-width" aria-label="Default select example">
@@ -161,41 +163,41 @@ const Register = () => {
 
                     <div className="form-group">
                         <label for="exampleInputPassword1">Provincia</label>
-                        <input 
-                        {...province}
-                        type="text
+                        <input
+                            {...province}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Provincia" />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Ciudad</label>
-                        <input 
-                        {...city}
-                        type="text" className="form-control" id="exampleInputPassword1" placeholder="Ciudad" />
+                        <input
+                            {...city}
+                            type="text" className="form-control" id="exampleInputPassword1" placeholder="Ciudad" />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Codigo Postal</label>
-                        <input 
-                        {...zipCode}
-                        type="text
+                        <input
+                            {...zipCode}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Codigo Postal" />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Dirección</label>
-                        <input 
-                         {...direction}
-                        type="text
+                        <input
+                            {...direction}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Direccion" />
                     </div>
 
                     <hr />
 
-                </form>
+                </div>
 
-                <form>
+                <div>
                     <h2>Información adicional</h2>
 
                     {/*  nivel educativootro select */}
-                    <select {...education}className="form-select select-width" aria-label="Default select example">
+                    <select {...education} className="form-select select-width" aria-label="Default select example">
                         <option selected>...</option>
                         <option >Secundario Incompleto</option>
                         <option >Secundario Completo</option>
@@ -207,9 +209,9 @@ const Register = () => {
                     {/* experiencia laboral */}
                     <div className="form-group">
                         <label for="exampleInputPassword1">Experiencia Laboral</label>
-                        <input 
-                        {...workExperience}
-                        type="text
+                        <input
+                            {...workExperience}
+                            type="text
                         " className="form-control" id="exampleInputPassword1" placeholder="Experiencia Laboral" />
                     </div>
                     {/* cargar un archivo */}
@@ -217,11 +219,12 @@ const Register = () => {
 
                     <hr />
 
-                    <button type="submit" className="btn btn-danger">Confirmar Registro</button>
                     <hr />
-                </form>
+                </div>
+                <button type="submit" className="btn btn-danger">Confirmar Registro
+                    </button>
                 <Link to='/'><a> Volver atrás </a></Link>
-            </div>
+            </form>
 
         </div>
     )
